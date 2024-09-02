4c6c2f8cd2efa178d3e9c0a30784390fe09f5c63
garvitmathur <mathurv716@gmail.com>
2024-09-02 16:16:23 +0530

    sign/signup form and login page and header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+ 
+ const Header = () => {
+   return (
+     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+        <img 
+         className='w-44'
+        src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png"/>
+       
+       
+       
+        
+     </div>
+   );
+ };
+  export default Header;
