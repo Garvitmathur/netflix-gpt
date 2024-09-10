68b00af671e82261cf8f7f86bc306bd1f2a3a51c
garvitmathur <mathurv716@gmail.com>
2024-09-10 12:20:08 +0530

    update2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,2 @@
 export const LOGO= "https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png"
 export const USER_AVATAR="https://lh3.googleusercontent.com/ogw/AF2bZyg1DWk4jJDQ4Xt6na9japqxLU-hbHmFRBGTxeXoLCTtPw=s32-c-mo"
-export const API_OPTIONS={
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjJkNGU3NGNmMDkxMjMyMzdlNGNlYjg3YzY1NzMyNyIsIm5iZiI6MTcyNTYyNjE1OS4xMjg5MjIsInN1YiI6IjY2ZGFmMTA1MWE2OTQ1MmUxNWQ0ZTM2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dVKDyc9ksVNdik5kS4Q62eHbBDLtjOrsmeI3jrLrrww'
-    }
-};
